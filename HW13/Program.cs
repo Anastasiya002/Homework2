db01f3e80cb9a86aa1938330920973406dced730
anastasiya <anastasiya.kovpak@gmail.com>
2022-11-19 20:44:23 +0300

    HW13

---
+++ HW13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else 
+{
+int DigitCount = (int)Math.Log10(number) + 1;
+int preresult = number / (int)Math.Pow(10, DigitCount - 3);
+int result = preresult % 10;   
+Console.WriteLine($"{number} -> {result}");
+}
