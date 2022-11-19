27e072ad75c75d5cc896f1f29e7536936c36b3bb
anastasiya <anastasiya.kovpak@gmail.com>
2022-11-19 20:44:42 +0300

    HW15

---
+++ HW15/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите день недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+   Console.Write($"{number} -> да"); 
+}
+else if (number < 1 || number > 7)
+{
+    Console.Write("Неверное значение"); 
+}
+else if (number > 1 || number < 7)
+{
+   Console.Write($"{number} -> нет"); 
+}
